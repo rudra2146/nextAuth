f3b2c060d4804a753aaf1f71fa804101f23854ee
Rudra Pandya <viralpandya079@gmail.com>
2025-04-15 20:47:49 +0530

    Profile page -ID

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,17 +1,20 @@
 'use client'
 
-
 import React from 'react'
+import { useParams } from 'next/navigation'
 
-function page({params}:any) {
+const ProfilePage = () => {
+  const params = useParams()
+  const id = params?.id
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <h1>Profile page</h1>
       <h2 className='p-3 bg-green-500 rounded text-black'>
-        {params.id}
+        {id}
       </h2>
     </div>
   )
 }
 
-export default page
+export default ProfilePage
